5d2565b9a0856c4a9ee58ee333bf7eb0bab32af7
MiheretB <miheretberhanu0@gmail.com>
2024-11-24 07:33:45 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import Home from "./pages/Home/Home.jsx"
+function App() {
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  )
+}
+export default App
